a3e3bc2d4185a4b2635a49cb461b61b54f6ae3d6
uumanzor18 <uzziel.umanzor@cablecolor.net>
2025-10-29 10:32:29 -0600

    cambio url

--- appx/config/config.php
+++ appx/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://api.parking.local/';
+//$config['base_url'] = 'http://api.parking.local/';
+$config['base_url'] = 'http://http://parkingapi-production-f209.up.railway.app/';
 //$config['base_url'] = 'https://wscbdev.cablecolor.hn/';
 
 /*
